d89bf161a4cf9c6121cadb84d24b7b7d382cf2a6
BOB450 <bwvoskuil@gmail.com>
2021-08-16 21:24:22 -0600

    new lazer code
    lazer will follow player y cardiants

--- Dream Runner/Assets/Scripts/Hunter.cs
+++ Dream Runner/Assets/Scripts/Hunter.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Hunter : MonoBehaviour
 {
     public int speed;
+    public Transform player;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +15,12 @@ public class Hunter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+
         transform.Translate(Vector2.right * Time.deltaTime * speed);
+        Vector3 pos = transform.position;
+
+        // transform.position = player.position.y;
+        transform.position = new Vector3(pos.x, player.position.y, 0);
     }
 }
