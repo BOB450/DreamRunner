fe6250035b911affc9881e5d1fc13ea85b2e23be
BOB450 <bwvoskuil@gmail.com>
2021-08-17 17:55:21 -0600

    game now saves total coins collected and saves and removes coins that have been picked up between sessions

--- Dream Runner/Assets/Scripts/CoinController.cs
+++ Dream Runner/Assets/Scripts/CoinController.cs
@@ -6,18 +6,55 @@ using UnityEngine.UI;
 public class CoinController : MonoBehaviour
 {
     public Text txt;
-    int coins;
+    public int coins;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        coins = PlayerPrefs.GetInt("NumCoins");
     }
 
     // Update is called once per frame
     void Update()
     {
         txt.text = coins.ToString();
+        //---------------------------------------------------------------------------------------------------------------------
+        //check if coin is picked up
+        if (PlayerPrefs.GetInt("coin") == 1)
+        {
+            GameObject go = GameObject.Find("coin");
+            //if the coin exist then destroy it
+            if (go)
+            {
+                Destroy(go.gameObject);
+                Debug.Log(name + "has been destroyed.");
+            }
+        }
+
+        if (PlayerPrefs.GetInt("coin (1)") == 1)
+        {
+            GameObject go = GameObject.Find("coin (1)");
+            //if the coin exist then destroy it
+            if (go)
+            {
+                Destroy(go.gameObject);
+                Debug.Log(name + "has been destroyed.");
+            }
+        }
+
+        if (PlayerPrefs.GetInt("coin (2)") == 1)
+        {
+            GameObject go = GameObject.Find("coin (2)");
+            //if the coin exist then destroy it
+            if (go)
+            {
+                Destroy(go.gameObject);
+                Debug.Log(name + "has been destroyed.");
+            }
+        }
+        //----------------------------------------------------------------------------------------------------------------------
     }
+    
 
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -28,8 +65,31 @@ public class CoinController : MonoBehaviour
 
             
             coins = coins + 1;
-            Debug.Log(coins);
+            PlayerPrefs.SetInt("NumCoins", coins);
+           // Debug.Log(coins);
             Destroy(collision.gameObject);
+            Debug.Log(collision.gameObject.name);
+
+            //--------------------------------------------------------------------------------------------------------------------
+            if (collision.gameObject.name == "coin")
+            {
+                PlayerPrefs.SetInt("coin", 1);
+                Debug.Log("yay");
+            }
+
+            if (collision.gameObject.name == "coin (1)")
+            {
+                PlayerPrefs.SetInt("coin (1)", 1);
+                Debug.Log("yay1");
+            }
+
+            if (collision.gameObject.name == "coin (2)")
+            {
+                PlayerPrefs.SetInt("coin (2)", 1);
+                Debug.Log("yay1");
+            }
+            //--------------------------------------------------------------------------------------------------------------------
+
             //gameObject.GetComponent<Renderer>().enabled = false;
             //gameObject.GetComponent<CircleCollider2D>().enabled = false;
             //gameObject.transform.localScale = new Vector3(0, 0, 0);
