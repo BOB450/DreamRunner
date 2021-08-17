13e6d6630985dd9691e28854ef11e6f285235718
BOB450 <bwvoskuil@gmail.com>
2021-08-16 23:59:11 -0600

    door added with end screen

---
+++ Dream Runner/Assets/Scripts/door.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class door : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("you won");
+
+        if (collision.gameObject.tag == "player")
+        {
+            SceneManager.LoadScene(2);
+            
+        }
+    }
+
+}
