d17f640cbf049ffa043a0a70aba8c18cf62d345e
BOB450 <bwvoskuil@gmail.com>
2021-08-19 14:26:05 -0600

    lots, more level signs

--- Dream Runner/Assets/Scripts/BoulderCpntroller.cs
+++ Dream Runner/Assets/Scripts/BoulderCpntroller.cs
@@ -7,11 +7,12 @@ public class BoulderCpntroller : MonoBehaviour
 
     public GameObject boulder;
     public GameObject spawnP;
+    public float spawnRate = 2.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("spawn", 1f, 2.5f);  //1s delay, repeat every 1s
+        InvokeRepeating("spawn", 1f, spawnRate);  //1s delay, repeat every 1s
     }
 
     // Update is called once per frame
